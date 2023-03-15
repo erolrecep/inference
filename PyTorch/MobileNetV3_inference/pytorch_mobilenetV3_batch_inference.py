69249049195161d99d13f2c7beadf5fb942b16bd
Recep Erol <recep.90@gmail.com>
2023-03-15 08:47:59 -0500

    inference script is updated with log info recordings

--- PyTorch/MobileNetV3_inference/pytorch_mobilenetV3_batch_inference.py
+++ PyTorch/MobileNetV3_inference/pytorch_mobilenetV3_batch_inference.py
@@ -10,6 +10,8 @@
 # import required libraries
 import sys
 import os
+import time
+from datetime import datetime
 import numpy as np
 from torchvision import models
 from torchvision.models.mobilenetv3 import MobileNet_V3_Small_Weights
@@ -47,27 +49,39 @@ batch_size = 32
 
 dataloader = MyDataLoader(images_path, batch_size)
 
-# TODO: Write experiment results to a csv file (make sure each experiment have their own experiment folder)
+experiment_time = str(datetime.now())
+experiment_folder = f"experiments/PyTorch_MobileNetV3_{args['device']}_{batch_size}_{experiment_time}"
+if not os.path.exists(f"{experiment_folder}"):
+    os.mkdir(f"{experiment_folder}")
+
+processing_times = []
+
 # TODO: Grad-Cam results are also going to be in this folder
 with torch.no_grad():
+    with open(f"{experiment_folder}/inference_results.txt", "w") as f:
+        f.write("image_path, predicted_label\n")
+        # TODO: I should record inference time for each batch
+        for batch, paths in dataloader:
+            timer_start = time.process_time()
+            # Move the batch of images to the device
+            batch = batch.to(device)
 
-    # TODO: I should record inference time for each batch
-    for batch, paths in dataloader:
-        # Move the batch of images to the device
-        batch = batch.to(device)
+            # Perform inference on the batch
+            with torch.no_grad():
+                outputs = model(batch)
 
-        # Perform inference on the batch
-        with torch.no_grad():
-            outputs = model(batch)
+            # Convert the outputs to probabilities using softmax
+            probabilities = torch.nn.functional.softmax(outputs, dim=1)
 
-        # Convert the outputs to probabilities using softmax
-        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+            # Get the predicted class labels
+            predicted_labels = torch.argmax(probabilities, dim=1)
 
-        # Get the predicted class labels
-        predicted_labels = torch.argmax(probabilities, dim=1)
+            # Print the predicted labels and the corresponding image paths
+            for i in range(len(paths)):
+                # print(f"Image path: {paths[i]}, Predicted label: {predicted_labels[i]}")
+                f.write(f"{paths[i]}, {predicted_labels[i]}\n")
+            timer_stop = time.process_time()
+            print(f"Batch process time is {round(timer_stop-timer_start, 2)} secs")
+            processing_times.append(round(timer_stop-timer_start, 2))
 
-        # Print the predicted labels and the corresponding image paths
-        for i in range(len(paths)):
-            print(f"Image path: {paths[i]}, Predicted label: {predicted_labels[i]}")
-
-        # TODO: I should learn how to send inference results to CPU to process and store them in a csv file
+# TODO: Write script configuration into a yaml file??? and save it to experiment folder
