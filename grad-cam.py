eba49456c19b69faa60ef0d8e23e2a18e8b01d7c
Recep Erol <recep.90@gmail.com>
2023-04-11 23:40:43 -0500

    grad-cam experiments

--- grad-cam.py
+++ grad-cam.py
@@ -1,42 +1,27 @@
-import numpy as np
-import torch
 from pytorch_grad_cam import GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from torchvision.models import resnet50
-from torchvision.models.resnet import ResNet50_Weights
-from torchvision import transforms
-from PIL import Image
-import cv2
 
-
-model = resnet50(weights=ResNet50_Weights.DEFAULT)
+model = resnet50(pretrained=True)
 target_layers = [model.layer4[-1]]
-
-IMAGE_PATH = "/home/xuser/PycharmProjects/inference/images/awot6aes00291.jpg"
-
-image = Image.open(IMAGE_PATH)
-image_cv2 = cv2.imread(IMAGE_PATH)
-
-transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )])
-
-image_transformed = transform(image)  # Create an input tensor image for your model..
+input_tensor = # Create an input tensor image for your model..
 # Note: input_tensor can be a batch tensor with several images!
 
-input_tensor = torch.unsqueeze(image_transformed, 0)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-input_tensor = input_tensor.to(device)
-
 # Construct the CAM object once, and then re-use it on many images:
-cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
+cam = GradCAM(model=model, target_layers=target_layers, use_cuda=args.use_cuda)
+
+# You can also use it within a with statement, to make sure it is freed,
+# In case you need to re-create it inside an outer loop:
+# with GradCAM(model=model, target_layers=target_layers, use_cuda=args.use_cuda) as cam:
+#   ...
+
+# We have to specify the target we want to generate
+# the Class Activation Maps for.
+# If targets is None, the highest scoring category
+# will be used for every image in the batch.
+# Here we use ClassifierOutputTarget, but you can define your own custom targets
+# That are, for example, combinations of categories, or specific outputs in a non standard model.
 
 targets = [ClassifierOutputTarget(281)]
 
@@ -45,4 +30,4 @@ grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
 
 # In this example grayscale_cam has only one image in the batch:
 grayscale_cam = grayscale_cam[0, :]
-visualization = show_cam_on_image(image_transformed, grayscale_cam, use_rgb=True)
+visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
---
+++ grad-cam_benchmark_w_images_grad-cam.py
@@ -0,0 +1,70 @@
+from pytorch_grad_cam import GradCAM, GradCAMPlusPlus, EigenGradCAM, AblationCAM, RandomCAM
+import cv2
+from PIL import Image
+import numpy as np
+from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputSoftmaxTarget
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.metrics.road import ROADCombined
+from torchvision import models
+import imutils
+import torch
+
+
+# Showing the metrics on top of the CAM :
+def visualize_score(visualization, score, name, percentiles):
+    visualization = cv2.putText(visualization, name, (10, 20),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+    return visualization
+
+
+def benchmark(model, input_tensor, target_layers, eigen_smooth=False, aug_smooth=False, category=281):
+    methods = [("GradCAM", GradCAM(model=model, target_layers=target_layers, use_cuda=True))]
+
+    cam_metric = ROADCombined(percentiles=[20, 40, 60, 80])
+    targets = [ClassifierOutputTarget(category)]
+    metric_targets = [ClassifierOutputSoftmaxTarget(category)]
+
+    visualizations = []
+    percentiles = [10, 50, 90]
+    for name, cam_method in methods:
+        with cam_method:
+            attributions = cam_method(input_tensor=input_tensor,
+                                      targets=targets, eigen_smooth=eigen_smooth, aug_smooth=aug_smooth)
+        attribution = attributions[0, :]
+        scores = cam_metric(input_tensor, attributions, metric_targets, model)
+        score = scores[0]
+        visualization = show_cam_on_image(cat_and_dog, attribution, use_rgb=True)
+        visualization = visualize_score(visualization, score, name, percentiles)
+        visualizations.append(visualization)
+    return Image.fromarray(np.hstack(visualizations))
+
+
+model = models.resnet50(pretrained=True)
+model.eval()
+model.cuda()
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+images_path = "images"
+
+import os
+import sys
+sys.path.append(os.getcwd())
+from utils import list_images
+
+images_path = list(list_images(images_path))
+
+count = 0
+for image_path in images_path:
+    cat_and_dog = np.array(Image.open(image_path))
+    cat_and_dog = imutils.resize(cat_and_dog, 224, 224)
+    cat_and_dog = np.float32(cat_and_dog) / 255
+    input_tensor = preprocess_image(cat_and_dog, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    target_layers = [model.layer4]
+
+    input_tensor = input_tensor.cuda()
+    np.random.seed(42)
+    output = benchmark(model, input_tensor, target_layers, eigen_smooth=False, aug_smooth=False)
+    output.save(f"output2/grad-cam_benchmark{count}.png")
+    count += 1
